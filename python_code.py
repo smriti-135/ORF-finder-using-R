dda0ef19f0dfdf2d2b7fb84922d41d35a02fa66c
smriti-135 <129586221+smriti-135@users.noreply.github.com>
2024-09-23 10:55:46 +0530

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -9,24 +9,19 @@ codons = {
     'ACT': 'Thr','ACC': 'Thr','ACA': 'Thr','ACG': 'Thr','TGG': 'Trp','TAT': 'Tyr','TAC': 'Tyr','GTT': 'Val','GTC': 'Val','GTA': 'Val',
     'GTG': 'Val','TAA': '','TAG': '','TGA': ''
 }
-### To put your own FASTA file into the prog, de-comment the following code and comment the 24th and 25th line ###
-
-file = open('fourmil.fasta','r')
-newlist=[]
-seq=''
-listtwo=[]
-for lines in file:
-    count=0
-    if '\n' in lines:
-        newlist.append(lines)
-for i in range(1,len(newlist)-1):
-    seq+=newlist[i]
-result = "".join(line.strip() for line in seq.splitlines())
-
-# results='gatgttcgggggcgcctgtgcgaagccgaatgtccttaccctggttaattgtctaagtcagttatttaacatcgtcacgggtgacacgatcgctttgagctattataagtgtcagactgccacttaccgttgatagtggtcaatgtattt'
-# result = results.upper()
-
-
+# file = open('sequence 2.fasta','r')
+# newlist=[]
+# seq=''
+# listtwo=[]
+# for lines in file:
+#     count=0
+#     if '\n' in lines:
+#         newlist.append(lines)
+# for i in range(1,len(newlist)-1):
+#     seq+=newlist[i]
+# result = "".join(line.strip() for line in seq.splitlines())
+results='gatgttcgggggcgcctgtgcgaagccgaatgtccttaccctggttaattgtctaagtcagttatttaacatcgtcacgggtgacacgatcgctttgagctattataagtgtcagactgccacttaccgttgatagtggtcaatgtattt'
+result = results.upper()
 ####### functions ########
 def translation(final_string):
         points=0
@@ -44,7 +39,6 @@ def translation(final_string):
             pass
         else:    
             return translated
-        
 def spaces(f, result):
     seq_pos = ""
     count = 0
@@ -56,20 +50,13 @@ def spaces(f, result):
             seq_pos += " "
     return seq_pos
 
-def del_spaces(seq):
-    for i in range(len(seq)):
-        if seq[i]==' ':
-            seqnew=seq.replace(' ', '')
-    return(seqnew)
-    
-
-def driver_main(count, result, number):
+def driver_main(count, result):
     for f in range(3):
         seq_pos = spaces(f, result)
         if count==0:
-            print(f"The +{f+1} reading frame is: {seq_pos}\n")
+            print(f"The +{f+1} reading frame is: {seq_pos}")
         elif count==1:
-            print(f"The -{f+1} reading frame is: {seq_pos}\n")
+            print(f"The -{f+1} reading frame is: {seq_pos}")
         orfs=[]
         point=0
         found=1
@@ -81,19 +68,16 @@ def driver_main(count, result, number):
                     for j in range((i+3),len(seq_pos)):
                         if seq_pos[j]=='T'and seq_pos[j+1]=='A' and seq_pos[j+2]=='G':
                             point+=1
-                            signal=1 
                             break
                         elif seq_pos[j]=='T'and seq_pos[j+1]=='G' and seq_pos[j+2]=='A':
                             point+=1
-                            signal=2
                             break
                         elif seq_pos[j]=='T'and seq_pos[j+1]=='A' and seq_pos[j+2]=='A':
                             point+=1
-                            signal=3
                             break
                         else:
                             final_string+=seq_pos[j]
-                    if found>1:
+                    if found>1 and found%found==0:
                         orfs.append('ATG'+final_string)
                         final_string=''
             except:
@@ -101,36 +85,29 @@ def driver_main(count, result, number):
         if point!=0:
             try:
                 for i, orf in enumerate(orfs):
-                    new_orf=del_spaces(orf)
-                    if len(new_orf) > number:
-                        if signal==1:
-                            print(f'The ORFs {i+1} is: {orf}TAG\n')
-                        elif signal==2:
-                            print(f'The ORFs {i+1} is: {orf}TGA\n')
-                        elif signal==3:
-                            print(f'The ORFs {i+1} is: {orf}TAA\n')
-                        print(f'The amino acid sequence for ORF{i+1} is: {translation(orf)}\n')
-                    else:
-                        continue
+                    print(f'The ORFs {i+1} is: {orf}')
+                    print()
+                    print(f'The amino acid sequence for ORF{i+1} is: {translation(orf)}')
+                    print()
             except:
                     print()
-                    print("*******************There was an unknown error in translation at ORF, report this to dev.**********************\n")
+                    print("*******************There was an unknown error in translation at ORF, report this to dev.**********************")
                     print()
                     pass
         elif point==0:
-            print("No reading frames were found\n")
+            print("No reading frames were found")
+            print()
             pass
         if 'ATG' not in seq_pos:
-            print("No reading frames were found\n")
-    return " "
+            print("No reading frames were found")
+            print()
+    return 
 count=0
-print("Minimal ORF length 'nt' \n 1. 30 \n 2. 75 \n 3. 150 \n 4. 300 \n 5. 600 \n")
-number=int(input("Enter the value: "))
-print(driver_main(count, result, number))
+print(driver_main(count, result))
 comp = str.maketrans("ATGC", "TACG")
 complement = result.translate(comp)
 count=1
-print(driver_main(count, complement, number))
+print(driver_main(count, complement))
 end_time = time.perf_counter()
 execution_time = end_time - start_time
 print(f"Execution time: {execution_time:.5f} seconds")
